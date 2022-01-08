272efbdbd25d83181dbc9c46ff15fc394b804ff2
nicolas <apple@Apples-MacBook-Air.local>
2022-01-08 14:54:53 +0800

    fix: remove useless state and button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,17 +4,13 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 
 function ExpenseItem({ date, title, amount }) {
-  const [titleState, setTitle] = useState(title);
-  const clickHandler = () => setTitle('newTitle');
-
   return (
     <div className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{titleState}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
